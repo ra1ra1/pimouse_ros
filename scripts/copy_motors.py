f539a1ca5051a11b06292fa2d5e221cfb57c3eab
Watanabe Kisho <wlight0219@gmail.com>
2022-09-24 13:39:01 +0000

    Add new files

--- scripts/copy_motors.py
+++ scripts/copy_motors.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 #encording: utf8
 import sys, rospy, math
+from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
+from pimouse_ros.srv import TimedMotion
 
 class Motor():
     def __init__(self): #initialization
         if not self.set_power(True): sys.exit(1)
 
         rospy.on_shutdown(self.set_power) #Switching the power of the motor
+        self.sub_raw = rospy.Subscriber('motor_raw', MotorFreqs, self.callback_raw_freq)
         self.sub_cmd_vel = rospy.Subscriber('cmd_vel', Twist, self.callback_cmd_vel)
     	self.srv_on = rospy.Service('motor_on', Trigger, self.callback_on)
     	self.srv_off = rospy.Service('motor_off', Trigger, self.callback_off)
+	self.srv_tm = rospy.Service('timed_motion', TimedMotion, self.callback_tm)        
         self.last_time = rospy.Time.now()
         self.using_cmd_vel = False
     
@@ -25,19 +29,22 @@ class Motor():
         except:
             rospy.logerr("cannot write to " + en)
 
-        return False 
+        return False
 
     def set_raw_freq(self, left_hz, right_hz):
-	if not self.is_on:
+        if not self.is_on:
             rospy.logerr("not enpowered")
             return
         
-        lf = open("/dev/rtmotor_raw_l0", 'w')
-        lf.write(str(int(round(left_hz))) + "\n")
-        lf.close()
-        rf = open("/dev/rtmotor_raw_r0", 'w')
-        rf.write(str(int(round(right_hz))) + "\n")
-        rf.close()
+        try:
+            with open("/dev/rtmotor_raw_l0", 'w') as lf, open("/dev/rtmotor_raw_r0", 'w') as rf:
+                lf.write(str(int(round(left_hz))) + "\n")
+                rf.write(str(int(round(right_hz))) + "\n")
+        except:
+                rospy.logerr("cannot write to rtmotor_raw_*")
+
+    def callback_raw_freq(self, message):
+	self.set_raw_freq(message.left_hz, message.right_hz)
 
     def callback_cmd_vel(self, message):
 	forward_hz = 80000.0*message.linear.x / (9*math.pi)
@@ -54,6 +61,21 @@ class Motor():
 	
     def callback_on(self, message): return self.onoff_response(True)
     def callback_off(self, message): return self.onoff_response(False)
+
+    def callback_tm(self, message):
+	if not self.is_on:
+	    rospy.logerr("not enpowerd")
+	    return False
+	dev = "/dev/rtmotor0"
+	try:
+	    with open(dev, 'w') as f:
+		f.write("%d %d %d\n" % (message.left_hz, message.right_hz, message.duration_ms))
+	except:
+	    rospy.logerr("cannot write to" + dev)
+	    return False
+
+	return True
+
 
 if __name__ == '__main__':
     rospy.init_node('motors')
